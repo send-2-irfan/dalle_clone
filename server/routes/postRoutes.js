61133ea053a592395c618add75571ae9e66851bd
send-2-irfan <irfanullah.bses19@iba-suk.edu.pk>
2023-03-30 13:31:57 +0500

    models and routes added

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,8 @@
+import express  from "express";
+import * as dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
+import PostSchema from "../mongodb/models/post";
+
+dotenv.config();
+
+const router = express.Router();
